e1bbb3f3c57fb0e82ebd93b4bea187fcde162647
Rogger Ortiz <rogger.ortiz.br@gmail.com>
2023-10-26 18:55:14 -0500

    fixes

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -1,49 +1,44 @@
-import {
-  BanknotesIcon,
-  ClockIcon,
-  UserGroupIcon,
-  InboxIcon,
-} from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
+import { lusitana } from '@/app/ui/fonts'
+import { BanknotesIcon, ClockIcon, InboxIcon, UserGroupIcon } from '@heroicons/react/24/outline'
 
 const iconMap = {
   collected: BanknotesIcon,
   customers: UserGroupIcon,
   pending: ClockIcon,
-  invoices: InboxIcon,
-};
+  invoices: InboxIcon
+}
 
 export default async function Cards() {
   return (
     <>
-      <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
       <Card
         title="Total Customers"
         value={numberOfCustomers}
         type="customers"
-      />
+      /> */}
     </>
-  );
+  )
 }
 
 export function Card({
   title,
   value,
-  type,
+  type
 }: {
-  title: string;
-  value: number | string;
-  type: 'invoices' | 'customers' | 'pending' | 'collected';
+  title: string
+  value: number | string
+  type: 'invoices' | 'customers' | 'pending' | 'collected'
 }) {
-  const Icon = iconMap[type];
+  const Icon = iconMap[type]
 
   return (
-    <div className="rounded-xl bg-gray-50 p-2 shadow-sm">
-      <div className="flex p-4">
-        {Icon ? <Icon className="h-5 w-5 text-gray-700" /> : null}
-        <h3 className="ml-2 text-sm font-medium">{title}</h3>
+    <div className='rounded-xl bg-gray-50 p-2 shadow-sm'>
+      <div className='flex p-4'>
+        {Icon ? <Icon className='h-5 w-5 text-gray-700' /> : null}
+        <h3 className='ml-2 text-sm font-medium'>{title}</h3>
       </div>
       <p
         className={`${lusitana.className}
@@ -52,5 +47,5 @@ export function Card({
         {value}
       </p>
     </div>
-  );
+  )
 }
--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -1,16 +1,12 @@
-import { ArrowPathIcon } from '@heroicons/react/24/outline';
-import clsx from 'clsx';
-import Image from 'next/image';
-import { lusitana } from '@/app/ui/fonts';
+import { lusitana } from '@/app/ui/fonts'
+import { ArrowPathIcon } from '@heroicons/react/24/outline'
 export default async function LatestInvoices() {
   return (
-    <div className="flex w-full flex-col md:col-span-4 lg:col-span-4">
-      <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
-        Latest Invoices
-      </h2>
-      <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
-        <div className="bg-white px-6">
-          {latestInvoices.map((invoice, i) => {
+    <div className='flex w-full flex-col md:col-span-4 lg:col-span-4'>
+      <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>Latest Invoices</h2>
+      <div className='flex grow flex-col justify-between rounded-xl bg-gray-50 p-4'>
+        <div className='bg-white px-6'>
+          {/* {latestInvoices.map((invoice, i) => {
             return (
               <div
                 key={invoice.id}
@@ -44,13 +40,13 @@ export default async function LatestInvoices() {
                 </p>
               </div>
             );
-          })}
+          })} */}
         </div>
-        <div className="flex items-center pb-2 pt-6">
-          <ArrowPathIcon className="h-5 w-5 text-gray-500" />
-          <h3 className="ml-2 text-sm text-gray-500 ">Updated just now</h3>
+        <div className='flex items-center pb-2 pt-6'>
+          <ArrowPathIcon className='h-5 w-5 text-gray-500' />
+          <h3 className='ml-2 text-sm text-gray-500 '>Updated just now</h3>
         </div>
       </div>
     </div>
-  );
+  )
 }
