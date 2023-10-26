9516dc898f60ddcb74a106291b82a1e5d3e7d0b6
Rogger Ortiz <rogger.ortiz.br@gmail.com>
2023-10-26 18:37:48 -0500

    chapters 1-5

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,10 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import { inter } from '@/app/ui/fonts'
+import '@/app/ui/global.css'
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang='en'>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
-  );
+  )
 }
---
+++ app/login/page.tsx
@@ -0,0 +1,11 @@
+import LoginForm from '../ui/login-form'
+
+export default function LoginPage() {
+  return (
+    <div className='flex justify-center items-center h-full'>
+      <div className='w-50'>
+        <LoginForm />
+      </div>
+    </div>
+  )
+}
