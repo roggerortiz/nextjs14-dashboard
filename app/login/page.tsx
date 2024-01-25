f850d1c4d103de9f0070b107af57e0403081b0ea
Rogger Ortiz <rogger.ortiz.br@gmail.com>
2024-01-24 22:54:15 -0500

    authentication

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,21 @@
 import AcmeLogo from '@/app/ui/acme-logo';
-import LoginForm from '@/app/ui/login-form';
+import dynamic from 'next/dynamic';
+
+const LoginForm = dynamic(() => import('@/app/ui/login-form'), {
+  ssr: false,
+  loading: () => (
+    <div className="space-y-3">
+      <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
+        Loading...
+      </div>
+    </div>
+  ),
+});
 
 export default function LoginPage() {
   return (
     <main className="flex items-center justify-center md:h-screen">
-      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
+      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4">
         <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
           <div className="w-32 text-white md:w-36">
             <AcmeLogo />
